69791ea264ce1bd4e3b4106d130f52e1c6bd476a
卢伟锦 <60027985+CrazyDogJ@users.noreply.github.com>
2026-08-09 21:53:40 +0800

    update

---
+++ Source/FriendsJourney/Private/GAS/GameplayAbility_Base.cpp
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GAS/GameplayAbility_Base.h"
+
+void UGameplayAbility_Base::OnAvatarSet(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+	Super::OnAvatarSet(ActorInfo, Spec);
+	
+	if (ActorInfo->PlayerController.IsValid())
+	{
+		if (ActorInfo->IsLocallyControlledPlayer())
+		{
+			OnLocallyControlled();
+		}
+	}
+	
+	// Controller change delegate.
+	if (const auto Pawn = Cast<APawn>(ActorInfo->AvatarActor))
+	{
+		Pawn->ReceiveControllerChangedDelegate.AddUniqueDynamic(this, &ThisClass::ReceiveControllerChanged);
+	}
+	
+	K2_OnAvatarSet(*ActorInfo, Spec);
+}
+
+void UGameplayAbility_Base::OnRemoveAbility(const FGameplayAbilityActorInfo* ActorInfo,
+                                            const FGameplayAbilitySpec& Spec)
+{
+	if (const auto Pawn = Cast<APawn>(ActorInfo->AvatarActor))
+	{
+		Pawn->ReceiveControllerChangedDelegate.RemoveAll(this);
+	}
+	
+	if (ActorInfo->IsLocallyControlledPlayer())
+	{
+		OnLocallyUncontrolled();
+	}
+	
+	K2_OnRemoveAbility(*ActorInfo, Spec);
+	
+	Super::OnRemoveAbility(ActorInfo, Spec);
+}
+
+void UGameplayAbility_Base::ReceiveControllerChanged(APawn* Pawn, AController* OldController,
+	AController* NewController)
+{
+	if (NewController && NewController->IsLocalPlayerController())
+	{
+		OnLocallyControlled();
+	}
+	else
+	{
+		OnLocallyUncontrolled();
+	}
+}
---
+++ Source/FriendsJourney/Public/GAS/GameplayAbility_Base.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GameplayAbility_Base.generated.h"
+
+UCLASS()
+class FRIENDSJOURNEY_API UGameplayAbility_Base : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void OnAvatarSet(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+	
+	virtual void OnRemoveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+	
+	// Begin play (On ability give) function for ga.
+	UFUNCTION(BlueprintImplementableEvent, DisplayName = "On Avatar Set")
+	void K2_OnAvatarSet(const FGameplayAbilityActorInfo ActorInfo, const FGameplayAbilitySpec& Spec);
+	
+	// End play function for ga.
+	UFUNCTION(BlueprintImplementableEvent, DisplayName = "On Remove Ability")
+	void K2_OnRemoveAbility(const FGameplayAbilityActorInfo ActorInfo, const FGameplayAbilitySpec& Spec);
+	
+	// Used for ui display.
+	UFUNCTION(BlueprintImplementableEvent, DisplayName = "On Locally Controlled")
+	void OnLocallyControlled();
+	
+	// Used for ui display.
+	UFUNCTION(BlueprintImplementableEvent, DisplayName = "On Locally Uncontrolled")
+	void OnLocallyUncontrolled();
+	
+protected:
+	UFUNCTION()
+	void ReceiveControllerChanged(APawn* Pawn, AController* OldController, AController* NewController);
+};
